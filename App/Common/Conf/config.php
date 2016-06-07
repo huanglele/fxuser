896ba374823b5a41ad64cad53a27ced2bddd4bcb
ihuanglele <844649263@qq.com>
2016-06-07 23:51:35 +0800

    添加了证书信息

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -22,8 +22,8 @@ return array(
         'AppSecret' => 'b4bf70c90f7975e72d78b7803697e434',
         'Token' => 'Z60z6Z6Q1aavK30K0GVv460t30bnA606',       //微信Token(令牌)
         'EncodingAESKey' => 'HdJJKSjx0kqcheREd1zYqJnSy4OCcRHeKdJyj2hECSH',//微信消息加解密密钥
-        'key' => '',
-        'mch_id' => '', //商户号
+        'key' => '123456789012345678901234567890rz',
+        'mch_id' => '1267553601', //商户号
         'notify_url' => 'http://' . $_SERVER['HTTP_HOST'] . '/index.php/wechat/notify',
         'SSLCERT_PATH' => LIB_PATH . "Org/Wxpay/apiclient_cert.pem",
         'SSLKEY_PATH' => LIB_PATH . "Org/Wxpay/apiclient_key.pem",
