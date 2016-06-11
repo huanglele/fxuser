ca600b021637b6ca49e199006098695f6841a013
ihuanglele <844649263@qq.com>
2016-06-12 00:29:18 +0800

    添加了后台账单信息，前台企业付款功能

--- ThinkPHP/Library/Org/Wxpay/WxRedPack.class.php
+++ ThinkPHP/Library/Org/Wxpay/WxRedPack.class.php
@@ -282,8 +282,8 @@ class WxRedPack
             curl_setopt($ch,CURLOPT_PROXYPORT, C('Wx.CURL_PROXY_PORT'));
         }
         curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);//严格校验
         //设置header
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         //要求结果为字符串且输出到屏幕上
